1acd7b49ea1c2f514858b17c1dac00ae783cc28f
ImanMousaei <imanmousaei1379@gmail.com>
2023-09-04 18:00:31 +0330

    Fix models

--- exchange/models.py
+++ exchange/models.py
@@ -4,6 +4,10 @@ from django.utils.translation import ugettext_lazy as _
 
 class Coin(models.Model):
     name = models.CharField(max_length=255, verbose_name=_('Coin Name'))
-    symbol = models.CharField(max_length=255, verbose_name=_('Coin Symbol'))
+    symbol = models.CharField(max_length=255, unique=True, verbose_name=_('Coin Symbol'))
     to_buy = models.FloatField(verbose_name=_('Residual coins that we need to buy when it exceeds 10'))
-    price = models.FloatField(verbose_name=_('Coin Price in USD'))
+    price_in_usdt = models.FloatField(verbose_name=_('Coin Price in USDT'))
+
+class CoinBalance(models.Model):
+    coin = models.ManyToManyField(Coin)
+    balance = models.FloatField(verbose_name=_('Coin Balance'))
--- users/models.py
+++ users/models.py
@@ -2,6 +2,8 @@ from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
 from django.db import models
 
+from exchange.models import CoinBalance
+
 
 class Customer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -9,6 +11,8 @@ class Customer(models.Model):
     national_id = models.CharField(max_length=10, null=True, blank=True, verbose_name=_('Melli Code'))
     city = models.CharField(max_length=30, null=True, blank=True, verbose_name=_('City'))
     level = models.PositiveIntegerField(default=1, verbose_name=_('Authentication Level'))
+    
+    coin_balance = models.ForeignKey(CoinBalance, verbose_name=_('Balance'))
 
     def __str__(self):
         if self.user.first_name or self.user.last_name:
